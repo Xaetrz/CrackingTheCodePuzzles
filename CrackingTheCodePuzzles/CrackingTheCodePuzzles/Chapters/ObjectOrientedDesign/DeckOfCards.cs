fa959ff1e2f54ccb534e77779fd4f4a63ad175af
jkress <jonathank1993@gmail.com>
2021-01-03 02:01:59 -0600

    OOP Scratch

---
+++ CrackingTheCodePuzzles/CrackingTheCodePuzzles/Chapters/ObjectOrientedDesign/DeckOfCards.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrackingTheCodePuzzles.Chapters.ObjectOrientedDesign
+{
+    public enum Suit { Spade, Heart, Club, Diamond }
+
+    public abstract class Deck<T> where T: Card
+    {
+        private List<T> _cards;  // Card, dealt or otherwise
+        public int _dealtIndex = 0;  // Index of last dealt card
+
+        public void Shuffle() { /*Generic Implementation. Rearrange _cards and reset _dealtIndex*/ }
+        public T DealCard() { /*Generic Implementation.*/ throw new NotImplementedException(); } 
+    }
+
+    public abstract class Card
+    {
+        public Suit Suit { get; private set; }
+        public int FaceValue { get; private set; }
+
+        public Card(Suit suit, int val)
+        {
+            this.Suit = suit;
+            this.FaceValue = val;
+        }
+    }
+
+    public abstract class Hand<T> where T: Card
+    {
+        private List<Card> _hand;
+
+        public Hand() { _hand = new List<Card>(); }
+
+        public abstract int Score();
+        public void AddCard(Card card)
+        {
+            _hand.Add(card);
+        }
+    }
+
+
+
+    public class Blackjack
+    {
+        Deck<>
+    }
+}
