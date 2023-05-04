2789157dd29c27345072d019694fcc71f771e3db
jkress <jonathank1993@gmail.com>
2023-05-04 18:37:09 -0500

    Fix fib start

--- CrackingTheCodePuzzlesJS/OtherPuzzles/FibonacciSeries.js
+++ CrackingTheCodePuzzlesJS/OtherPuzzles/FibonacciSeries.js
@@ -3,10 +3,10 @@
 */
 
 function fibonacciIterative(num) {
-    if (num <= 1) return 1;
+    if (num <= 2) return 1;
     let curr = 1;
     let prev = 1;
-    for (let i = 2; i <= num; i++) {
+    for (let i = 3; i <= num; i++) {
         let temp = curr;
         curr += prev;
         prev = temp;
@@ -15,7 +15,7 @@ function fibonacciIterative(num) {
 }
 
 function fibonacciRecursive(num) {
-    if (num <= 1) return 1;
+    if (num <= 2) return 1;
     return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
 }
 
